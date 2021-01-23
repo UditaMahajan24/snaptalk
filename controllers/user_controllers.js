73d5372ed1b4c46659fbed22a85ad7694648b15f
udita <udita mahajan 24@gmail.com>
2021-01-24 01:03:26 +0530

    user sign-up done

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,5 +1,62 @@
+const User=require('../models/user');
+
 module.exports.profile=function(req,res){
     return res.render('user_profile',{
     title:"user profile"
     });
 }
+
+// for sign in
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+    title:"user sign_in"
+    });
+}
+// for sign up
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+    title:"user sign_Up"
+    });
+}
+// for getting data of sign up 
+module.exports.create=function(req,res){
+    if(req.body.password != req.body.confirm)
+    {
+    return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},function(err,user){
+        if(err)
+        {
+            console.log("error in finding the user in signing up");
+            return
+        }
+        if(!user)
+        {
+            User.create(req.body,function(err,user){
+                if(err){
+              console.log("error in creating  the user while signing up");
+               return
+             }
+             return res.redirect('/user/sign-in')
+            });
+        }
+        else
+        {
+            return res.redirect('back');
+        }
+    });
+}
+
+// for getting data of sign in
+module.exports.createSession=function(req,res){
+}
+
+
+
+
+
+
+
+
+
+
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,11 @@ const express=require('express');
 const router= express.Router();
 const usercontroller=require('../controllers/user_controllers');
 router.get('/profile',usercontroller.profile);
+router.get('/sign-in',usercontroller.signIn);
+router.get('/sign-up',usercontroller.signUp);
+router.post('/create',usercontroller.create);
+
+
 
 
 
